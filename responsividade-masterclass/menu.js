2a1942cc93c61d9c6fbd298f29ec44af100dcbba
alfredo <tito.alfred@hotmail.com>
2020-07-30 11:01:36 -0300

    Responsividade na Prática  - Masterclass #08

---
+++ responsividade-masterclass/menu.js
@@ -0,0 +1,11 @@
+/* estratégias para responsividades */
+let show = true;
+
+const menuSection = document.querySelector(".menu-section");
+const menuToggle = menuSection.querySelector(".menu-toggle");
+
+menuToggle.addEventListener("click", () => {
+  document.body.style.overflow = show ? "hidden" : "initial";
+  menuSection.classList.toggle("on", show);
+  show = !show;
+});
